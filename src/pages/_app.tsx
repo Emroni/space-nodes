f1516ead3918284e119abc9b4ae3825e9fe71bc9
Emre Koc <hi@emrekoc.io>
2023-04-08 11:41:37 +0100

    Add Material UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Box, CssBaseline, ThemeProvider } from '@mui/material';
+import type { AppProps } from 'next/app';
+import theme from '../theme';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+    return <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box component="main" padding={4}>
+            <Component {...pageProps} />
+        </Box>
+    </ThemeProvider>;
+
 }
---
+++ src/theme.ts
@@ -0,0 +1,10 @@
+import { createTheme } from '@mui/material';
+
+export default createTheme({
+    palette: {
+        mode: 'dark',
+    },
+    typography: {
+        fontFamily: 'Roboto,Helvetica,Arial,sans-serif',
+    },
+});
